2834278ea73a4ac016921e6d7191efa715fe99ca
gaoq <672405020@qq.com>
2021-02-22 14:42:50 +0800

    修改seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,8 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            ArticleSeeder::class,
             \Encore\Admin\Auth\Database\AdminTablesSeeder::class,
+            ArticleSeeder::class,
         ]);
     }
 }
